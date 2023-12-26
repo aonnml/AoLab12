ea731ffc8f063b6d566b8fe1e0f1802f5c256d22
aonnml <radchanon694@gmail.com>
2023-12-26 15:27:13 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(long long int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(long long int x){
+    if(x <= 1){
+        return x;
+    }else{
+        return fibonacci(x-1) + fibonacci(x-2);
+    }
 }
